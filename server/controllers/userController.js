dcdc38c0c8ff64dbbfc77494d4cabc344e076d69
YinWei <cba5530@gmail.com>
2021-11-08 16:59:46 +0800

    feat: finish basic user controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,21 +1,108 @@
+const Post = require('../models/post')
+const User = require('../models/user')
+const Apply = require('../models/apply')
+
 const userController = {
-  signUp: (req, res) => {
+  signUp: async (req, res) => {
+    try {
+      if (!req.body.email || !req.body.name || !req.body.password || !req.body.checkPassword) {
+        return res.json({ message: 'input cannot be blank' })
+      }
+      if (req.body.checkPassword !== req.body.password) {
+        return res.json({ message: 'Password is different' })
+      }
 
+      const user = await User.findOne({ email: req.body.email })
+      if (user) return res.json({ message: 'Email is already exists', })
+
+      const { name, email, password } = req.body
+
+      await User.create({
+        name,
+        email,
+        password
+      })
+      return res.json({ status: 'success', message: 'User was successfully registered' })
+    } catch (err) {
+      console.log(err)
+      return res.json({ message: 'Internal Server Error' })
+    }
   },
-  signIn: (req, res) => {
+  signIn: async (req, res) => {
+    try {
+      if (!req.body.email || !req.body.password) {
+        return res.json({ message: "required fields didn't exist" })
+      }
 
+      const { email, password } = req.body
+      const user = await User.findOne({email: email })
+
+      if (!user) return res.json({ message: "user not found" })
+
+      if (password !== user.password) return res.json({ message: "password is not correct" })
+
+      res.json({
+        status: 'success',
+        message: 'ok',
+        user: {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          nickname: user.nickname
+        }
+      })
+    } catch (err) {
+      console.log(err)
+      res.json({ message: 'Internal Server Error' })
+    }
   },
   getUserPosts: (req, res) => {
-    res.send('getUserPosts')
+    Post.find({ userId: req.params.id })
+      .then(posts => {
+        return res.json(posts)
+      })
+      .catch(err => {
+        console.log(err)
+        return res.json({ message: 'Internal Server Error' })
+      })
   },
   getUserApplies: (req, res) => {
-    res.send('getUserApplies')
+    Apply.find({ userId: req.params.id })
+      .then(replies => {
+        return res.json(replies)
+      })
+      .catch(err => {
+        console.log(err)
+        return res.json({ message: 'Internal Server Error' })
+      })
   },
   getUser: (req, res) => {
-    res.send('getUser')
+    User.findById(req.params.id)
+      .then(user => {
+        return res.json(user)
+      })
+      .catch(err => {
+        console.log(err)
+        return res.json({ message: 'Internal Server Error' })
+      })
   },
   putUser: (req, res) => {
-
+    User.findById(req.params.id)
+      .then(user => {
+        const { test, name, nickname, phone, lineId, email, password } = req.body
+        user.name = name || user.name
+        user.nickname = nickname || user.nickname
+        user.phone = phone || user.phone
+        user.lineId = lineId || user.lineId
+        user.email = email || user.email
+        user.password = password || user.password
+        user.save()
+        return res.json(user)
+      })
+      .catch(err => {
+        console.log(err)
+        return res.json({ message: 'Internal Server Error' })
+      })
   }
 }
 
