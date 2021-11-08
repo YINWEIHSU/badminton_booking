f189b8eefc704b7637f70608f3e01536edec2837
YinWei <cba5530@gmail.com>
2021-11-08 22:45:56 +0800

    feat: finish basic applyController

--- server/controllers/applyController.js
+++ server/controllers/applyController.js
@@ -1,15 +1,57 @@
-const applyController = {
-  postApply: (req, res) => {
+const Apply = require('../models/apply')
+const User = require('../models/user')
+const Post = require('../models/post')
 
+
+const applyController = {
+  postApply: async (req, res) => {
+    try {
+      const { numberOfPeople, remarks } = req.body
+      await Apply.create({
+        userId: '6187fe519d2a9f4bdd1aeb5f',
+        postId: req.params.post_id,
+        numberOfPeople,
+        remarks,
+      })
+      return res.json({ message: 'Successfully created' })
+    } catch (err) {
+      console.log(err)
+      return res.json({ message: 'Internal Server Error' })
+    }
   },
   getApplies: (req, res) => {
-    res.send('getApplies')
+    Apply.find()
+      .then(applies => {
+        return res.json(applies)
+      })
+      .catch(err => {
+        console.log(err)
+        return res.json({ message: 'Internal Server Error' })
+      })
   },
   getApply: (req, res) => {
-    res.send('getApply')
+    Apply.findById(req.params.id).then(apply => {
+      return res.json(apply)
+    }).catch(err => {
+      console.log(err)
+      return res.json({ message: 'Internal Server Error' })
+    })
   },
-  putApply: (req, res) => {
+  putApply: async (req, res) => {
+    try {
+      const { numberOfPeople, remarks, isConfirmed } = req.body
 
+      const apply = await Apply.findById(req.params.id)
+      apply.numberOfPeople = numberOfPeople
+      apply.remarks = remarks
+      apply.isConfirmed = isConfirmed
+      apply.save()
+
+      return res.json({ message: 'Successfully updated' })
+    } catch (err) {
+      console.log(err)
+      return res.json({ message: 'Internal Server Error' })
+    }
   }
 }
 
