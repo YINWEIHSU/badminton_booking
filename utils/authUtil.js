415c8e52c54f190b8ca8093aa8d2166ee111a598
subaruuuu <meteorbs13@gmail.com>
2021-12-22 23:46:32 +0800

    feat: implement redis feature

--- utils/authUtil.js
+++ utils/authUtil.js
@@ -1,18 +1,28 @@
 const { createHmac } = require('crypto')
+const { redisUtils } = require('../utils/redisUtil')
 
-function verifyToken(req, res, next) {
-  if (req.headers.authorization.startsWith("Bearer ")) {
-    const token = req.headers.authorization.split(" ")[1]
+async function verifyToken(req, res, next) {
+  const checkBearer = req.headers.authorization.startsWith("Bearer ")
+
+  if (!checkBearer) {
+    return new Error('authorization not support!')
+  }
+  const cacheResult = await redisUtils.getCaches('token')
+  const token = req.headers.authorization.split(" ")[1]
+
+  if (!!cacheResult && cacheResult === token) {
     const jwtToken = authUtil.deconstructToken(token)
     const hmac = createHmac(process.env.JWT_ALGO, process.env.JWT_SECRET);
     const sign = hmac.update(`${jwtToken.headerReq}.${jwtToken.contentReq}`).digest('hex')
+
     if (sign === jwtToken.signReq) {
       next()
     } else {
-      return new Error('request not valid!')
+      return new Error('token not valid!')
     }
   } else {
-    return new Error('authorization not support!')
+    //之後要改成 redirect
+    return res.json({ message: 'authorization denied' })
   }
 }
 
